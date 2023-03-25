c64e94342cfe064760af539db0776b710044b31d
jacobshk <50532381+jacobshk@users.noreply.github.com>
2023-03-25 16:30:02 -0400

    functional structure commmit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Green_Neighborhood
 HooHacks
+ng v = 15.2.4
+node v = 18.5.0
+npm v = 9.5.0
--- green-neighborhood/src/app/app.component.spec.ts
+++ green-neighborhood/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'green-neighborhood-website'`, () => {
+  it(`should have as title 'green-neighborhood'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('green-neighborhood-website');
+    expect(app.title).toEqual('green-neighborhood');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('green-neighborhood-website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('green-neighborhood app is running!');
   });
 });
